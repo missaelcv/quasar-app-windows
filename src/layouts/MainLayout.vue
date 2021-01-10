08bc169eea828f598ce651f90d0844da78b11626
MisaWeb <elbilpapi@gmail.com>
2021-01-10 18:07:24 -0400

    Nuevo Quasar Windows

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="leftDrawerOpen = !leftDrawerOpen" />
+
+        <q-toolbar-title>
+         Todo Windows
+        </q-toolbar-title>
+
+        <div>Quasar missaelcv</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <q-list>
+        <q-item-label header class="text-grey-8">
+         Menu de Navegaciones
+
+         <q-item clickable to="/">
+         <q-item-section avatar>
+           <q-icon name="school"></q-icon>
+         </q-item-section>
+        <q-item-section>
+       <q-item-label>Documentos</q-item-label>
+       <q-item-label caption>quasar.dev</q-item-label>
+         </q-item-section>   
+         </q-item>
+        
+          </q-item-label> <EssentialLink v-for="link in essentialLinks" :key="link.title" 
+          v-bind="link" />
+        </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center q-pa-md">
+      <br>
+    <h3 class="flex flex-center">Lorem</h3>
+
+     </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
